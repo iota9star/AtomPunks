3fb8bf1bba10ada533b8541b1e8d4b96feedaf6a
iota9star <iota9star@foxmail.com>
2023-10-13 14:05:06 +0800

    ✨ Add pull

---
+++ pull.js
@@ -0,0 +1,26 @@
+import shell from 'shelljs';
+import chalk from "chalk";
+
+if (!shell.which('git')) {
+    shell.echo('Sorry, this script requires git');
+    shell.exit(1);
+}
+
+while (true) {
+    console.log(chalk.blue("Pulling from main..."));
+    shell.exec("git pull origin main")
+
+    console.log(chalk.blue("Adding upstream..."));
+    shell.exec("git remote add upstream https://github.com/raymondwang0225/AtomPunks")
+
+    console.log(chalk.blue("Fetching upstream..."));
+    shell.exec("git fetch upstream")
+
+    console.log(chalk.blue("Merging upstream..."));
+    shell.exec("git merge upstream/main")
+
+    console.log(chalk.blue("Pushing to main..."));
+    shell.exec("git push origin main")
+
+    await new Promise(r => setTimeout(r, 60 * 1000));
+}
